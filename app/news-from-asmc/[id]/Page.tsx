2066962c228c1a2df9a52b99f0d9268f7467ea4f
Alkoun-web-design <hassam.sajid.dvlpr@gmail.com>
2025-11-25 15:37:46 +0500

    remove blog page

--- app/news-from-asmc/[id]/Page.tsx
+++ app/news-from-asmc/[id]/Page.tsx
@@ -1,7 +1,7 @@
 import Cta2 from "@/components/Cta2";
 import ContactUs from "@/components/ContactUs";
-import Blog from '@/components/Blog';
-import { posts } from '@/data/posts';
+// import Blog from '@/components/Blog';
+// import { posts } from '@/data/posts';
 export default async function Page({
   params,
 }: {
@@ -23,27 +23,27 @@ export default async function Page({
 }
 
 
-// Generate static paths for all posts
-export async function getStaticPaths() {
-  // Get all slugs from local data
-  const paths = posts.map(post => ({
-    params: { id: post.id }
-  }));
+// // Generate static paths for all posts
+// export async function getStaticPaths() {
+//   // Get all slugs from local data
+//   const paths = posts.map(post => ({
+//     params: { id: post.id }
+//   }));
 
-  return {
-    paths,
-    fallback: false // Show 404 for non-existent slugs
-  };
-}
+//   return {
+//     paths,
+//     fallback: false // Show 404 for non-existent slugs
+//   };
+// }
 
-// Fetch data for each post
-export async function getStaticProps({ params }: { params: Promise<{ id: number }> }) {
-  // Find the post with matching slug
-  const post = posts.find(post => post.id === params.id);
+// // Fetch data for each post
+// export async function getStaticProps({ params }: { params: Promise<{ id: number }> }) {
+//   // Find the post with matching slug
+//   const post = posts.find(post => post.id === params.id);
 
-  return {
-    props: {
-      post
-    }
-  };
-}
+//   return {
+//     props: {
+//       post
+//     }
+//   };
+// }
